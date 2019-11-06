199ad61d461e96928b49b646f5b5d3766b9950cb
Vanillasushi <43223181+Vanillasushi@users.noreply.github.com>
2019-11-06 17:36:12 -0600

    First commit.

---
+++ AssignmentsWebApp/Models/Accomplishment.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentsWebApp.Models
+{
+    public class Accomplishment
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string DateOfAccomplishment { get; set; }
+    }
+}
---
+++ AssignmentsWebApp/Models/FamousPerson.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentsWebApp.Models
+{
+    public class FamousPerson
+    {
+        public string FirstName { get; set; }
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public string City { get; set; }
+        public ICollection<Accomplishment> Accomplishments { get; set; }
+    }
+}
